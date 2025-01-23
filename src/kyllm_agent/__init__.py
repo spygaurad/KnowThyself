b8dcd9c30a2a17077be88262a3bd0162b28b391e
spygaurad <suraj.prasai97@gmail.com>
2025-01-23 20:12:52 +0545

    Restructure

---
+++ src/kyllm_agent/__init__.py
@@ -0,0 +1,10 @@
+"""
+KYLLM Agent
+
+This module defines a custom agent graph that allows user to understand their model better
+
+"""
+
+from kyllm_agent.graph import StateGraph
+
+__all__ = ["StateGraph"]
